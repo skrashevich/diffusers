a709876ccb6020c5cd67e12cb6040297ed081cbb
Sayak Paul <spsayakpaul@gmail.com>
2023-05-08 16:54:18 +0530

    more debug messages.

--- src/diffusers/models/attention_processor.py
+++ src/diffusers/models/attention_processor.py
@@ -718,6 +718,7 @@ class AttnProcessor2_0:
             raise ImportError("AttnProcessor2_0 requires PyTorch 2.0, to use it, please upgrade PyTorch to 2.0.")
 
     def __call__(self, attn: Attention, hidden_states, encoder_hidden_states=None, attention_mask=None):
+        print(f"Hidden states: {hidden_states.shape}")
         batch_size, sequence_length, _ = (
             hidden_states.shape if encoder_hidden_states is None else encoder_hidden_states.shape
         )
@@ -738,19 +739,23 @@ class AttnProcessor2_0:
 
         key = attn.to_k(encoder_hidden_states)
         value = attn.to_v(encoder_hidden_states)
+        print(f"Key and value: {key.shape}, {value.shape}")
 
         head_dim = inner_dim // attn.heads
         query = query.view(batch_size, -1, attn.heads, head_dim).transpose(1, 2)
         key = key.view(batch_size, -1, attn.heads, head_dim).transpose(1, 2)
         value = value.view(batch_size, -1, attn.heads, head_dim).transpose(1, 2)
+        print(f"Query, key, and value: {query.shape}, {key.shape}, {value.shape}")
 
         # the output of sdp = (batch, num_heads, seq_len, head_dim)
         # TODO: add support for attn.scale when we move to Torch 2.1
         hidden_states = F.scaled_dot_product_attention(
             query, key, value, attn_mask=attention_mask, dropout_p=0.0, is_causal=False
         )
+        print(f"Hidden states from efficient attention: {hidden_states.shape}")
 
         hidden_states = hidden_states.transpose(1, 2).reshape(batch_size, -1, attn.heads * head_dim)
+        print(f"Hidden states after transposes: {hidden_states.shape}")
         hidden_states = hidden_states.to(query.dtype)
 
         # linear proj
